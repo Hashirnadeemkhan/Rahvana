501bcabde4162a02d68b613baa82001ba103d27f
Hashir Nadeem Khan <khashir657@gmail.com>
2025-11-07 01:22:53 +0500

    module error solved

--- app/visa-forms/i130/wizard/page.tsx
+++ app/visa-forms/i130/wizard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import DynamicFormWizard from "@/app/components/DynamicForm";
-import { Loader } from "lucide-react";
+
 
 export default function Home() {
   return <DynamicFormWizard />;
