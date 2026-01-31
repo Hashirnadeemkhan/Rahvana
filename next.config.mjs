33b39a57f635368fc53de9c94a861b0dabaf10a7
Ghulam-Ahmed-1357 <ghulamahmedmbs@gmail.com>
2026-02-01 00:59:42 +0500

    ..

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
     config.resolve.fallback = { 
       ...config.resolve.fallback,
       canvas: false,
@@ -12,7 +19,7 @@ const nextConfig = {
     turbo: {
       resolve: {
         alias: {
-          canvas: './lib/empty.js',
+          canvas: path.join(__dirname, 'lib/mock-canvas.js'),
         },
       },
     },
